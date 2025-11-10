26be7e36cb16ee67ad04cf302039da11767b6a5e
Arslan Sultanov <kargrad@MacBook-Air-Arslan.local>
2025-11-11 01:07:50 +0500

    feat: implement Stack

---
+++ src/stack.js
@@ -0,0 +1,21 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
+
+module.exports = {
+  Stack
+};
