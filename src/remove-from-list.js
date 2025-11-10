5680e0670135005a407df7d4572d9e52a33b35a4
Arslan Sultanov <kargrad@MacBook-Air-Arslan.local>
2025-11-11 01:07:50 +0500

    feat: implement RemoveFromList

---
+++ src/remove-from-list.js
@@ -0,0 +1,21 @@
+function removeKFromList(l, k) {
+  while (l && l.value === k) {
+    l = l.next;
+  }
+
+  let current = l;
+
+  while (current && current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next; 
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
+}
+
+module.exports = {
+  removeKFromList
+};
